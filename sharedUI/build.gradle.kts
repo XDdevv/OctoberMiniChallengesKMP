0bd924d2893739c01045aa98e837a5f00b059eae
XDdevv <undefineduser087@gmail.com>
2025-10-08 22:18:37 +0500

    Initial commit

---
+++ sharedUI/build.gradle.kts
@@ -0,0 +1,89 @@
+import org.jetbrains.compose.ExperimentalComposeLibrary
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.compose.multiplatform)
+    alias(libs.plugins.android.kmp.library)
+    alias(libs.plugins.compose.hot.reload)
+    alias(libs.plugins.kotlinx.serialization)
+    alias(libs.plugins.buildConfig)
+}
+
+kotlin {
+    android {
+        namespace = "rainxch.zed.cctoberminichallengeskmp"
+        compileSdk = 36
+        minSdk = 23
+        androidResources.enable = true
+    }
+
+    jvm()
+
+    js { browser() }
+    wasmJs { browser() }
+
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(compose.runtime)
+            implementation(compose.ui)
+            implementation(compose.foundation)
+            implementation(compose.material3)
+            implementation(compose.components.resources)
+            implementation(compose.components.uiToolingPreview)
+            implementation(libs.kermit)
+            implementation(libs.kotlinx.coroutines.core)
+            implementation(libs.androidx.lifecycle.viewmodel)
+            implementation(libs.androidx.lifecycle.runtime)
+            implementation(libs.kotlinx.serialization.json)
+            implementation(libs.kotlinx.datetime)
+            implementation(libs.kstore)
+        }
+
+        commonTest.dependencies {
+            implementation(kotlin("test"))
+            @OptIn(ExperimentalComposeLibrary::class)
+            implementation(compose.uiTest)
+            implementation(libs.kotlinx.coroutines.test)
+        }
+
+        androidMain.dependencies {
+            implementation(compose.uiTooling)
+            implementation(libs.kotlinx.coroutines.android)
+            implementation(libs.kstore.file)
+        }
+
+        jvmMain.dependencies {
+            implementation(compose.desktop.currentOs)
+            implementation(libs.kotlinx.coroutines.swing)
+            implementation(libs.kstore.file)
+        }
+
+        webMain.dependencies {
+            implementation(libs.kstore.storage)
+        }
+
+        iosMain.dependencies {
+            implementation(libs.kstore.file)
+        }
+
+    }
+
+    targets
+        .withType<KotlinNativeTarget>()
+        .matching { it.konanTarget.family.isAppleFamily }
+        .configureEach {
+            binaries { framework { baseName = "SharedUI" } }
+        }
+}
+
+buildConfig {
+    // BuildConfig configuration here.
+    // https://github.com/gmazzo/gradle-buildconfig-plugin#usage-in-kts
+}
---
+++ webApp/src/commonMain/kotlin/main.kt
@@ -0,0 +1,6 @@
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.window.ComposeViewport
+import rainxch.zed.cctoberminichallengeskmp.App
+
+@OptIn(ExperimentalComposeUiApi::class)
+fun main() = ComposeViewport { App() }
