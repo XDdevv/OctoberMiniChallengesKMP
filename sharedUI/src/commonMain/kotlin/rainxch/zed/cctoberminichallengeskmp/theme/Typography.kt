7c3fc5f0e84f94722620d2deb5eceee9c78184c7
XDdevv <undefineduser087@gmail.com>
2025-10-10 20:29:41 +0500

    Added road rage font family

---
+++ sharedUI/src/commonMain/kotlin/rainxch/zed/cctoberminichallengeskmp/theme/Typography.kt
@@ -0,0 +1,6 @@
+package rainxch.zed.cctoberminichallengeskmp.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import octoberminichallengeskmp.sharedui.generated.resources.Res
+import octoberminichallengeskmp.sharedui.generated.resources.road_rage_regular
